76fbe3b507df7093c3aa171f63e3e7a479d7ee70
4piu <16668056+4piu@users.noreply.github.com>
2020-03-06 00:14:35 +0800

    Create tool template

---
+++ src/components/tools/Template.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {withStyles} from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import MyAppBar from "../MyAppBar";
+import Grid from "@material-ui/core/Grid";
+
+const styles = theme => ({});
+
+class ToolClass extends React.Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        changeActivity: PropTypes.func.isRequired
+    };
+
+    render() {
+        return (
+            <>
+                {/** App bar */}
+                <MyAppBar
+                    title={this.props.title}
+                    isHome={false}
+                    changeActivity={this.props.changeActivity}/>
+                {/** Main view */}
+                <Grid container>
+
+                </Grid>
+            </>
+        );
+    }
+}
+
+export default withStyles(styles)(ToolClass);
