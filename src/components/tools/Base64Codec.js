d0a3063406a23dd6938c530c10a31b590eb65a31
4piu <16668056+4piu@users.noreply.github.com>
2020-03-08 23:05:59 +0800

    Base64 Codec - completed

--- src/components/tools/Base64Codec.js
+++ src/components/tools/Base64Codec.js
@@ -2,7 +2,6 @@ import React from "react";
 import {withStyles} from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import MyAppBar from "../MyAppBar";
-import Container from "@material-ui/core/Container";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
@@ -10,8 +9,15 @@ import AddIcon from '@material-ui/icons/Add';
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from '@material-ui/icons/Close';
 import {v4 as uuidV4} from "uuid";
-import {shallowCompare} from "../../utils/ObjectCompare";
-import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import DoneIcon from "@material-ui/icons/Done";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import {downloadText} from "../../utils/DownloadService";
+// noinspection ES6UnusedImports
+import regeneratorRuntime from "regenerator-runtime";
 
 class DefaultTab {
     constructor() {
@@ -26,7 +32,7 @@ const styles = theme => ({
     Container: {
         padding: theme.spacing(3),
         '& > *': {
-            marginBottom: theme.spacing(3),
+            marginBottom: theme.spacing(2),
         }
     },
     TabsBar: {
@@ -51,10 +57,23 @@ const styles = theme => ({
     CloseTabButton: {
         marginLeft: "auto",
         padding: 0
-    }
+    },
+    ButtonContainer: {
+        '& > *': {
+            marginRight: theme.spacing(1),
+            marginBottom: theme.spacing(2)
+        }
+    },
+    ButtonWrapper: {
+        position: 'relative',
+        display: 'inline-flex',
+        [theme.breakpoints.down('xs')]: {
+            width: '100%'
+        }
+    },
 });
 
-class Base64Codec extends React.PureComponent {
+class Base64Codec extends React.Component {
     static propTypes = {
         title: PropTypes.string.isRequired,
         changeActivity: PropTypes.func.isRequired
@@ -64,7 +83,9 @@ class Base64Codec extends React.PureComponent {
         super(props);
         this.state = {
             tabIndex: "",
-            tabs: []
+            tabs: [],
+            copied: false,
+            focus: "decoded"
         }
     }
 
@@ -73,7 +94,7 @@ class Base64Codec extends React.PureComponent {
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (!shallowCompare(prevState, this.state, ['tabIndex'])) this.saveState();
+        this.saveState();
     };
 
     loadState = () => {
@@ -128,10 +149,57 @@ class Base64Codec extends React.PureComponent {
         });
     };
 
-    closeAllTab = event => {
+    closeAllTab = () => {
         this.setState({
             tabs: [new DefaultTab()]
         })
+    };
+
+    decodeInput = event => {
+        const input = event.currentTarget.value;
+        const decoded = Buffer.from(input, 'base64').toString('base64') === input ?
+            Buffer.from(input, 'base64').toString('utf-8') : 'Malformed input';
+        this.setState(prevState => {
+            const thisTab = prevState.tabs.find(({id}) => id === this.state.tabIndex);
+            thisTab.encoded = input;
+            thisTab.decoded = decoded;
+            return {
+                focus: "decoded",
+                tabs: prevState.tabs
+            }
+        })
+    };
+
+    encodeInput = event => {
+        const input = event.currentTarget.value;
+        const encoded = Buffer.from(input, 'utf-8').toString('base64');
+        this.setState(prevState => {
+            const thisTab = prevState.tabs.find(({id}) => id === this.state.tabIndex);
+            thisTab.encoded = encoded;
+            thisTab.decoded = input;
+            return {
+                focus: "encoded",
+                tabs: prevState.tabs
+            }
+        })
+    };
+
+    buttonCopyHandler = async () => {
+        const text = this.state.tabs.filter(({id}) => id === this.state.tabIndex)[0][this.state.focus];
+        await navigator.clipboard.writeText(text);
+        this.setState({
+            copied: true
+        });
+        setTimeout(() => {
+            this.setState({
+                copied: false
+            })
+        }, 3000);
+    };
+
+    buttonDownloadHandler = async () => {
+        const text = this.state.tabs.filter(({id}) => id === this.state.tabIndex)[0][this.state.focus];
+        downloadText(`${this.state.focus}.txt`, text);
     };
 
     render() {
@@ -181,18 +249,65 @@ class Base64Codec extends React.PureComponent {
                     </Tabs>
                 </AppBar>
                 {/** Main view */}
-                <Container className={classes.Container} maxWidth={"md"}>
-                    {this.state.tabs.map(tab => (
-                        <Typography
-                            key={tab.id}
-                            component="div"
-                            role="tabpanel"
-                            hidden={this.state.tabIndex !== tab.id}
-                        >
-                            {tab.name + ': ' + tab.id}
-                        </Typography>
-                    ))}
-                </Container>
+                <>
+                    {this.state.tabs
+                        .filter(({id}) => id === this.state.tabIndex)
+                        .map(tab => (
+                            <Grid className={classes.Container} container spacing={2} key={`grid-container-${tab.id}`}>
+                                <Grid item xs={12} md={6}>
+                                    <TextField
+                                        id="textarea-encoded"
+                                        label="Base64 Encoded"
+                                        multiline
+                                        fullWidth={true}
+                                        variant="outlined"
+                                        value={this.state.tabs.filter(({id}) => id === this.state.tabIndex)[0].encoded}
+                                        inputProps={{
+                                            spellCheck: false
+                                        }}
+                                        rows={16}
+                                        rowsMax={16}
+                                        onChange={this.decodeInput}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={6}>
+                                    <TextField
+                                        id="textarea-decoded"
+                                        label="Plain Text"
+                                        multiline
+                                        fullWidth={true}
+                                        variant="outlined"
+                                        value={this.state.tabs.filter(({id}) => id === this.state.tabIndex)[0].decoded}
+                                        inputProps={{
+                                            spellCheck: false
+                                        }}
+                                        rows={16}
+                                        rowsMax={16}
+                                        onChange={this.encodeInput}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} className={classes.ButtonContainer}>
+                                    <div className={classes.ButtonWrapper}>
+                                        <Button variant="contained"
+                                                onClick={this.buttonCopyHandler}
+                                                fullWidth={true}
+                                                startIcon={this.state.copied ? <DoneIcon/> : <FileCopyIcon/>}
+                                        >{this.state.copied ?
+                                            'Copied' :
+                                            `Copy ${this.state.focus}`
+                                        }</Button>
+                                    </div>
+                                    <div className={classes.ButtonWrapper}>
+                                        <Button variant="contained"
+                                                onClick={this.buttonDownloadHandler}
+                                                fullWidth={true}
+                                                startIcon={<GetAppIcon/>}
+                                        >{`Download ${this.state.focus}`}</Button>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                        ))}
+                </>
             </>
         );
     }
