17798f67969a2f77329e2cb1334574faf7270a8a
4piu <16668056+4piu@users.noreply.github.com>
2026-07-03 21:31:16 -0400

    GH actions for page deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({command}) => ({
+    base: command === "build" ? "/tool-bench/" : "/",
     define: {
         global: "globalThis"
     },
@@ -10,4 +11,4 @@ export default defineConfig({
         react(),
         tailwindcss()
     ]
-});
+}));
